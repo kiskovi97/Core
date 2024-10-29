cd489210ca4834eba72cc8135ae9cb670d14d703
GergoKovacs <kiskovi97@gmail.com>
2024-10-29 16:08:09 +0100

    Improve TriggerActionList

--- Core/Actions/TriggerActionList.cs
+++ Core/Actions/TriggerActionList.cs
@@ -1,13 +1,25 @@
-﻿namespace Kiskovi.Core
+﻿using UnityEngine;
+
+namespace Kiskovi.Core
 {
     internal class TriggerActionList : TriggerAction
     {
         public TriggerAction[] Actions;
+        public bool triggerActionsOfChildren = false;
+
         public override void Trigger(params object[] parameter)
         {
             foreach (var action in Actions)
             {
                 Trigger(action);
+            }
+
+            if (triggerActionsOfChildren)
+            {
+                foreach (Transform trans in transform)
+                {
+                    Trigger(trans.GetComponent<TriggerAction>());
+                }
             }
         }
     }
