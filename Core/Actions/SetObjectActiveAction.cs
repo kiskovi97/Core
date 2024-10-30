f56a85c28b7bdcff774137938b9431234f79a7f3
GergoKovacs <kiskovi97@gmail.com>
2024-10-30 14:19:05 +0100

    Add more functions

---
+++ Core/Actions/SetObjectActiveAction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Kiskovi.Core
+{
+    internal class SetObjectActiveAction : TriggerAction
+    {
+        public GameObject[] objects;
+
+        public bool isActive;
+
+        public override void Trigger(params object[] parameter)
+        {
+            foreach(var obj in objects)
+            {
+                obj.SetActive(isActive);
+            }
+        }
+    }
+}
---
+++ Core/Camera/ChangeCameraPriority.cs
@@ -0,0 +1,18 @@
+﻿using Cinemachine;
+
+namespace Kiskovi.Core
+{
+    internal class ChangeCameraPriority : TriggerAction
+    {
+        public CinemachineVirtualCamera virtualCamera;
+        public int priority;
+
+        public override void Trigger(params object[] parameter)
+        {
+            if (virtualCamera != null)
+            {
+                virtualCamera.Priority = priority;
+            }
+        }
+    }
+}
