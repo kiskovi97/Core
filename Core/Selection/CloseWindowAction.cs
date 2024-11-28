62eb58a8ce7b2eb4a2eacab06c3844fd589b0f1b
GergoKovacs <kiskovi97@gmail.com>
2024-11-28 14:44:24 +0100

    New things

---
+++ Core/Selection/CloseWindowAction.cs
@@ -0,0 +1,13 @@
+﻿namespace Kiskovi.Core
+{
+    internal class CloseWindowAction : TriggerAction
+    {
+        public UIWindow window;
+
+        public override void Trigger(params object[] parameter)
+        {
+            if (window != null)
+                window.Close();
+        }
+    }
+}
---
+++ Core/Selection/OpenWindowAction.cs
@@ -0,0 +1,13 @@
+﻿namespace Kiskovi.Core
+{
+    internal class OpenWindowAction : TriggerAction
+    {
+        public UIWindow window;
+
+        public override void Trigger(params object[] parameter)
+        {
+            if (window != null)
+                window.Open();
+        }
+    }
+}
