9194114180f8529b0fd627d0d70ca94295dedd74
GergoKovacs <kiskovi97@gmail.com>
2024-12-17 12:19:48 +0100

    Update

---
+++ Core/Interactive/RequestRefreshOnInteractionObject.cs
@@ -0,0 +1,12 @@
+﻿namespace Kiskovi.Core
+{
+    internal class RequestRefreshOnInteractionObject : TriggerAction
+    {
+        public InteractionObject target;
+
+        public override void Trigger(params object[] parameter)
+        {
+            target.RequestRefresh();
+        }
+    }
+}
