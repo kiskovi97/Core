9f41b3dba39b709eb6688e6da125dddccab9fc8b
GergoKovacs <kiskovi97@gmail.com>
2025-02-07 16:17:25 +0100

    Update Shadow Caster Creator

--- Core/ShadowCaster2DCreator.cs
+++ Core/ShadowCaster2DCreator.cs
@@ -61,6 +61,11 @@ public class ShadowCaster2DCreator : MonoBehaviour
             DestroyImmediate(child.gameObject);
         }
     }
+
+    public void OnEnable()
+    {
+        Create();
+    }
 }
 
 
