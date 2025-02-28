8a3c962035c28c0800f34584ce045cf4c7be79ea
GergoKovacs <kiskovi97@gmail.com>
2025-02-28 14:57:40 +0100

    Trigger Animation Action

---
+++ Core/Actions/TriggerAnimationAction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Kiskovi.Core
+{
+    internal class TriggerAnimationAction : TriggerAction
+    {
+        public Animator animator;
+        public string triggerName;
+        public override void Trigger(params object[] parameter)
+        {
+            animator.SetTrigger(triggerName);
+        }
+    }
+}
