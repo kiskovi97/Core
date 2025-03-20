1bbdee4a4d6a5541dd156c8bde9acc3b727e0558
GergoKovacs <kiskovi97@gmail.com>
2025-03-20 13:11:18 +0100

    Particles

---
+++ Core/Particles/ParticleTimer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Kiskovi.Core
+{
+    internal class ParticleTimer : MonoBehaviour
+    {
+        public ParticleSystem _particleSystem;
+
+        private float time;
+
+        public void AddTime(float time)
+        {
+            this.time = Mathf.Max(this.time, time);
+        }
+
+        private void Update()
+        {
+            if (_particleSystem == null) return;
+
+            if (time > 0)
+            {
+                time -= Time.deltaTime;
+                if (!_particleSystem.isPlaying)
+                    _particleSystem.Play();
+            } else
+            {
+                if (_particleSystem.isPlaying)
+                    _particleSystem.Stop();
+            }
+        }
+    }
+}
---
+++ Core/Particles/StartParticlesAction.cs
@@ -0,0 +1,13 @@
+﻿namespace Kiskovi.Core
+{
+    internal class StartParticlesAction : TriggerAction
+    {
+        public ParticleTimer particleTimer;
+        public float time = 1f;
+
+        public override void Trigger(params object[] parameter)
+        {
+            particleTimer.AddTime(time);
+        }
+    }
+}
