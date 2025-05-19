a8f1a025e4dfa8f2b4c2e9ed6f9d4ce6605ce19f
GergoKovacs <kiskovi97@gmail.com>
2025-05-19 14:07:27 +0200

    InputSystemTriggers

---
+++ Core/Input/InputSystemTriggers.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+namespace Kiskovi.Core
+{
+    internal class InputSystemTriggers : MonoBehaviour
+    {
+        public TriggerAction onAnyButtonpressed;
+
+        private void OnEnable()
+        {
+            InputSystem.onAnyButtonPress.CallOnce(ctrl => TriggerAction.Trigger(onAnyButtonpressed));
+        }
+    }
+}
