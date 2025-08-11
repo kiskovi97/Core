089a260b21af8f9b6912476cebadf00cae92e84c
GergoKovacs <kiskovi97@gmail.com>
2025-08-11 15:03:00 +0200

    spawb object

---
+++ Core/Actions/SpawnObjectAction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+using Zenject;
+
+namespace Kiskovi.Core
+{
+    internal class SpawnObjectAction : TriggerAction
+    {
+        public Transform target;
+        public GameObject prefab;
+
+        [Inject] private DiContainer container;
+
+        public override void Trigger(params object[] parameter)
+        {
+            base.Trigger(parameter);
+
+            if (container != null)
+            {
+                container.InstantiatePrefab(prefab, target.position, target.rotation, null);
+            } else
+            {
+                Instantiate(prefab, target.position, target.rotation);
+            }
+        }
+    }
+}
