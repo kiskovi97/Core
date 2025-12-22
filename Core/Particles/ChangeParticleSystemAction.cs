69c312f5344d16c9bf8c8f7d48faf3d1964fee45
Gergo Kovacs <kiskovi97@gmail.com>
2025-12-22 08:56:01 +0100

    Add particleSystem action

---
+++ Core/Particles/ChangeParticleSystemAction.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Kiskovi.Core
+{
+    internal class ChangeParticleSystemAction : TriggerAction
+    {
+        public ParticleSystem myParticleSystem;
+        public ParticleSystemChange change;
+        public enum ParticleSystemChange
+        {
+            Play,
+            Stop,
+        }
+
+        public override void Trigger(params object[] parameter)
+        {
+            switch(change)
+            {
+                case ParticleSystemChange.Play:
+                    myParticleSystem.Play();
+                    break;
+                case ParticleSystemChange.Stop:
+                    myParticleSystem.Stop();
+                    break;
+            }
+        }
+
+    }
+}
