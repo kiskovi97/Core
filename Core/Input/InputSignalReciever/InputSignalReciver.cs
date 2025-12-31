cee14aa65cc57e12cdc46978e9ed924c103f160a
Gergo Kovacs <kiskovi97@gmail.com>
2025-12-31 09:14:58 +0100

    Input Action Reciever

---
+++ Core/Input/InputSignalReciever/InputSignalReciver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+
+namespace Kiskovi.Core
+{
+    public class InputSignalReciver : MonoBehaviour
+    {
+        public TriggerAction onSignalRecived;
+
+        [HideInInspector] public string inputActionSignalTypeName;
+
+        private Type _cachedType;
+        protected Type CachedType
+        {
+            get
+            {
+                if (_cachedType == null)
+                    _cachedType = InputActionSignalTypeCache.GetByName(inputActionSignalTypeName);
+                return _cachedType;
+            }
+        }
+
+        [Inject] protected SignalBus _signalBus;
+
+        private void OnEnable()
+        {
+            _signalBus.Subscribe(CachedType, SignalCalled);
+        }
+
+        private void OnDisable()
+        {
+            _signalBus.TryUnsubscribe(CachedType, SignalCalled);
+        }
+
+        private void SignalCalled(object signal)
+        {
+            TriggerAction.Trigger(onSignalRecived);
+        }
+    }
+}
---
+++ Editor/Scripts/InputActionSignalReieverEditor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+using UnityEditor;
+
+using UnityEngine;
+
+namespace Kiskovi.Core
+{
+    [CustomEditor(typeof(InputSignalReciver), true, isFallback = true)]
+    internal class InputActionSignalReieverEditor : Editor
+    {
+        private int _selectedIndex;
+        private string[] _typeOptions;
+
+        private void OnEnable()
+        {
+            _typeOptions = InputActionSignalTypeCache.GetTypes(true).ToArray();
+
+            var targetScript = (InputSignalReciver)target;
+            _selectedIndex = Array.FindIndex(_typeOptions, name => name == targetScript.inputActionSignalTypeName);
+            if (_selectedIndex < 0) _selectedIndex = 0;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            var targetScript = (InputSignalReciver)target;
+
+            EditorGUILayout.LabelField("Input Action Signal Type");
+            _selectedIndex = EditorGUILayout.Popup(_selectedIndex, _typeOptions);
+
+            targetScript.inputActionSignalTypeName = _typeOptions[_selectedIndex];
+
+            if (GUI.changed)
+            {
+                EditorUtility.SetDirty(targetScript);
+            }
+        }
+    }
+}
