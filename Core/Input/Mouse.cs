1a205a2dad37c5198561c68af3289122a2abff20
Gergo Kovacs <kiskovi97@gmail.com>
2026-01-03 10:33:15 +0100

    MouseUtilities

---
+++ Core/Input/Mouse.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Kiskovi.Core
+{
+    public static class MouseUitilities
+    {
+        public static Vector2 GetMousePosition(RectTransform rectTransform)
+        {
+            Vector2 mousePosition;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                rectTransform,
+                Mouse.current.position.value,
+                null,
+                out mousePosition
+            );
+            return mousePosition;
+        }
+    }
+}
